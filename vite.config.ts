919c4f2e1545b51c55998c5992a57991671bc327
eric5654 <ericsantos2526@gmail.com>
2026-01-05 22:12:39 -0300

    correção

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Ministerio/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
